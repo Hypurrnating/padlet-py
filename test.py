603553c23d0a9353aff546e3170671556ab624de
Hypurrnating <imadyounis2004@gmail.com>
2024-08-17 01:54:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import pytest
 import asyncio
+import os
+os.system('pip install python-dotenv')
+import dotenv
+dotenv.load_dotenv()
+
+from .padlet import main
 
 def testit():
-    
+    asyncio.run(main.padlet(os.environ.get('api_key')).board())
